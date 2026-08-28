81ad8d14c24e7a1da72c04d445922001ab709120
Stephen Kell <srk31@srcf.ucam.org>
2026-08-28 12:45:49 +0100

    util.c: small fixes, and add (unused for now) fmt_hex_num_with_bitwidth

--- src/util.c
+++ src/util.c
@@ -1,7 +1,9 @@
+#include <stdint.h>
+
 __attribute__((visibility("hidden")))
-const char *fmt_hex_num(unsigned long n)
+const char *fmt_hex_num(uint64_t n)
 {
-	static char buf[19];
+	static char buf[19]; // FIXME: check if a problem to make this thread-local?
 	buf[0] = '0';
 	buf[1] = 'x';
 	signed i_dig = 15;
@@ -14,3 +16,24 @@ const char *fmt_hex_num(unsigned long n)
 	buf[18] = '\0';
 	return buf;
 }
+
+#if 0
+__attribute__((visibility("hidden")))
+const char *fmt_hex_num_with_bitwidth(uint64_t n, unsigned width_nbits)
+{
+	static char buf[35]; // FIXME: check if a problem to make this thread-local?
+	buf[0] = '0';
+	buf[1] = 'x';
+	unsigned pos = 2;
+	signed i_dig = width_nbits % 4 == 0 ? width_nbits / 4 : 1 + (width_nbits / 4);
+	if (i_dig > 31) i_dig = 31;
+	do
+	{
+		unsigned long long dig = (n >> (4 * i_dig)) & 0xf;
+		buf[pos++] = (dig > 9) ? ('a' + dig - 10) : ('0' + dig);
+		--i_dig;
+	} while (i_dig >= 0);
+	buf[pos] = '\0';
+	return buf;
+}
+#endif
